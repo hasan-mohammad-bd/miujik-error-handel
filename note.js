ffeae78e848922dbd92d47c266f26d0bc8f1888b
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-10 17:17:09 +0600

    first commit

---
+++ note.js
@@ -0,0 +1,28 @@
+/* 
+1. to see where the a function declared, ctrl and press on the function name.
+
+2. error line gives you just stating point of finding the error.
+
+3. console.log the bug point to see what is happening.
+
+4. if the id doesn't exist in the html element, the error will the null type.
+
+5. ctrl + f, now search the item. and with upper arrow you can go the immediate declared same value.
+
+6. always solve the upper error, the second error can be appear for the upper error reason.
+
+7. 404 means. the data can to be loaded.
+
+8. https:// must be included for before any api.
+
+9. reference error: the calling name or id is doesn't exist.
+
+10. use ternary operator if needed.
+
+11. use conditional chaining to to avoid error.
+
+12. clean the contentText 
+
+13. 
+
+*/
